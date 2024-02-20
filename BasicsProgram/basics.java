cefc471c71e1b10be7cee519ceaa567b13118ec4
adityakr1108 <aditya.kr.agrawal1108@gmail.com>
2024-02-21 02:30:54 +0530

    Initial Commit

--- BasicsProgram/basics.java
+++ BasicsProgram/basics.java
@@ -6,7 +6,8 @@ public class Basics {
     public static void main(String[] args){
         int arr[] =new int[50];
         Scanner sc = new Scanner(System.in);
-        for(int i = 0; i<=4;i++){
+        arr[6] = 50; 
+        for(int i = 0; i<=arr.length;i++){
             arr[i] = sc.nextInt();
         }
         for(int i = 0; i<=4;i++){
