16e193e8ce70edc3fa46abcb5371231aee31d60a
Aditya Kumar <aditya.kr.agrawal1108@gmail.com>
2024-10-20 23:28:54 +0530

    Update PairSum.java

--- ArrayList/PairSum.java
+++ ArrayList/PairSum.java
@@ -1,35 +1,35 @@
 package ArrayList;
 import java.util.*;
 
-
-/// First approach
+// for 
+// / First approach
 // Brute Force
-// public class PairSum {
-//     public static void main(String[] args) {
-//         ArrayList<Integer> list = new ArrayList<>();
-//         System.out.println("Enter the size of list...");
-//         Scanner sc = new Scanner(System.in);
-//         int size = sc.nextInt();
-//         System.out.println("Enter the elemnets to be inserted...");
-//         for(int i = 0; i<size;i++){
-//             int a = sc.nextInt();
-//             list.add(a);
-//         }
-//         System.out.println("Enter the target value...");
-//         int target = sc.nextInt();
-//          ArrayList<Integer> pairSum = new ArrayList<>();
-//         for(int i = 0;i<size-1;i++){
-//             for(int j = i+1;j<size;j++){
-//                 if(target == list.get(i) + list.get(j)){
-//                     pairSum.add(list.get(i));
-//                     pairSum.add(list.get(j));
-//                 }
-//             }
-//         }
-//         System.out.println(pairSum);
-//         sc.close();
-//     }
-// }
+public class PairSum {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println("Enter the size of list...");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        System.out.println("Enter the elemnets to be inserted...");
+        for(int i = 0; i<size;i++){
+            int a = sc.nextInt();
+            list.add(a);
+        }
+        System.out.println("Enter the target value...");
+        int target = sc.nextInt();
+         ArrayList<Integer> pairSum = new ArrayList<>();
+        for(int i = 0;i<size-1;i++){
+            for(int j = i+1;j<size;j++){
+                if(target == list.get(i) + list.get(j)){
+                    pairSum.add(list.get(i));
+                    pairSum.add(list.get(j));
+                }
+            }
+        }
+        System.out.println(pairSum);
+        sc.close();
+    }
+}
 public class PairSum {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
