69c89058906635c9bedc47b2aa17905829415847
adityakr1108 <aditya.kr.agrawal1108@gmail.com>
2024-11-23 13:29:11 +0530

    new commit

---
+++ Question/twoSum/twoSum.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class twoSum{
+public static void main(String[] args){
+    int arr[] = {2,7,11,15,6,3,5,4};
+    int target = 9;
+    Set<Integer>  pair = new HashSet<>();
+    for(int i = 0;i<)
+}
+}
